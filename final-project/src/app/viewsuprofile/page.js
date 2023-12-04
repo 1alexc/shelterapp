c62dab3f618c8361c68590bb11416595f12eb125
James Diffey <jamesdiffey8@gmail.com>
2023-12-04 14:11:15 +0000

    update

--- final-project/src/app/viewsuprofile/page.js
+++ final-project/src/app/viewsuprofile/page.js
@@ -3,16 +3,11 @@ import Image from 'next/image'
 import BackButton from "../components/BackButton.jsx"
 export default function ViewSUProfile() {
     return (
-        
         <main>
-
         <BackButton/>
             <h1>View SU Profile</h1>
             <Link href="/editsu">
                 <button>Edit Service User Button</button>
-            </Link>
-            <Link href="/">
-                <button>Sign Out Button</button>
             </Link>
         </main>
     )
