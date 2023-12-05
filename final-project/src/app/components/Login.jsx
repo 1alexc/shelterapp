04b19c9b224110fa46275a18e15bc9b728fa0b39
Eyres1313 <133891699+Eyres1313@users.noreply.github.com>
2023-12-05 11:25:22 +0000

    env complete

--- final-project/src/app/components/Login.jsx
+++ final-project/src/app/components/Login.jsx
@@ -8,12 +8,7 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY
-  //   "https://mywogncsdbookcotjlwd.supabase.co",
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15d29nbmNzZGJvb2tjb3RqbHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE2ODg2ODUsImV4cCI6MjAxNzI2NDY4NX0.RUDHAnsq43Jvqf_ttim1XNLItWhpr8ohgiEKmXBeH_o"
-);
+const supabase = createClient(supabaseURL, supabaseKey);
 
 export default function Login() {
   const [session, setSession] = useState(null);
