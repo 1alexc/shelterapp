c079336f4537709694acc25ad2b60b8a7cf7a866
Alexander Carr <1alexander.carr@gmail.com>
2023-12-05 14:37:46 +0000

    names displayed as buttons, potential errors in console

--- final-project/src/app/viewallsus/page.js
+++ final-project/src/app/viewallsus/page.js
@@ -14,32 +14,40 @@ const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(supaurl, supakey);
 
 export default function ViewAllSUs() {
-    const [data, setData] = useState(null);
-
+    const [info, setInfo] = useState(null);
+    const [data, setData] = useState([]);
+  
     useEffect(() => {
-        async function fetchData() {
-          try {
-            const { data, error } = await supabase.from('service_users').select('*');
-            console.log(data);
-            if (error) {
-              throw error;
-            }
-    
-            setData(data);
-          } catch (error) {
-            console.error('Error fetching data:', error.message);
+      async function fetchData() {
+        try {
+          const { data, error } = await supabase.from('service_users').select('*');
+          console.log(data);
+          if (error) {
+            throw error;
           }
+  
+          setInfo(data);
+          setData(data);
+        } catch (error) {
+          console.error('Error fetching data:', error.message);
         }
-    
-        fetchData();
-      }, []);
+      }
+  
+      fetchData();
+    }, []);
+  
+    console.log(info);
+  
     return (
-        <main>
-        <BackButton/>
-            <h1>View SU</h1>
-            <Link href="/viewsuprofile">
-                <button>View Specific Service User Button</button>
-            </Link>
-        </main>
-    )
-}
+      <main>
+        <BackButton />
+        <h1>View SU</h1>
+        {data.map(item => (
+          <button key={item.id}>{item.first_name} {item.last_name}</button>
+        ))}
+        <Link href="/viewsuprofile">
+          <button>View Specific Service User Button</button>
+        </Link>
+      </main>
+    );
+  }
