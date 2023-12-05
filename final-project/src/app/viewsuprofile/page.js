24a955bd6df838e36a4a59b07b69f7b44056f682
Sara Thampi <saraannthampi@gmail.com>
2023-12-05 12:24:11 +0000

    fetch data

--- final-project/src/app/viewsuprofile/page.js
+++ final-project/src/app/viewsuprofile/page.js
@@ -1,7 +1,39 @@
+"use client"
 import Link from 'next/link'
 import Image from 'next/image'
 import BackButton from "../components/BackButton.jsx"
+// pages/index.js or components/YourComponent.js
+import { createClient } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+
+
+
+const supaurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+const supabase = createClient(supaurl, supakey);
+
 export default function ViewSUProfile() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            // Replace 'your_table_name' with the actual table name
+            const { data, error } = await supabase.from('service_users').select('*');
+            console.log(data);
+            if (error) {
+              throw error;
+            }
+    
+            setData(data);
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+          }
+        }
+    
+        fetchData();
+      }, []);
     return (
         <main>
         <BackButton/>
