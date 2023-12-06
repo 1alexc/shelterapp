428271a75ac877e60eba73046cf7153d7a198e84
Alexander Carr <1alexander.carr@gmail.com>
2023-12-06 16:42:49 +0000

    dashboard styling

--- final-project/src/app/pagecomponents/DashboardComp.jsx
+++ final-project/src/app/pagecomponents/DashboardComp.jsx
@@ -13,20 +13,34 @@ export default function DashboardComp() {
 
       <div className="flexbox-container2">
         <div className="flexbox-item2">
-          <Link href="/displayallsu">
-            <button>View Service User Button</button>
+          <div className="button-container">
+            <Link href="/displayallsu">
+            <button><img src="/icons8-view-32.png" alt="View Service User Icon" />View Service User Button</button>
           </Link>
-          <Link href="/addsu">
-            <button>Add Service User Button</button>
+          </div>
+          <div className="button-container">
+            <Link href="/addsu">
+            <button><img src="icons8-add-male-user-64.png" alt="Add Service User Icon" />Add Service User Button</button>
           </Link>
+          </div>
+          <div className="button-container">
           <Link href="/referrallinks">
-            <button>Referral Link Button</button>
+            <button><img src="icons8-referral-24.png" alt="Referral Link Button" />Referral Links</button>
           </Link>
+          </div>
+          <div className="button-container">
           <Link href="./">
-            <button>Sign Out Button</button>
+            <button><img src="icons8-sign-out-50.png" alt="Sign out Button" />Sign Out Button</button>
           </Link>
+          </div>
         </div>
       </div>
+    {/* <div className="flexbox-container">
+          <div className="flexbox-item"></div>
+          <div className="flexbox-item"></div>
+          <div className="flexbox-item"></div>
+          <div className="flexbox-item"></div>
+    </div}
     </>
   );
 }
