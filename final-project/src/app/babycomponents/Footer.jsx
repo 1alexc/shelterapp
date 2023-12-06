a2bdaf841ccfa416983a05e097be84fe63f649ae
Alexander Carr <1alexander.carr@gmail.com>
2023-12-06 14:29:36 +0000

    footer basic styling

--- final-project/src/app/babycomponents/Footer.jsx
+++ final-project/src/app/babycomponents/Footer.jsx
@@ -2,8 +2,8 @@ import babyCompStyles from "./footer.css"
 export default function Footer() {
     return (
         <>
-            <div className="rat">
-                <p>Powered by Redbull</p>
+            <div className="footer">
+                <p className="copyright">© Coders4Change</p>
             </div>
         </>
     )}
