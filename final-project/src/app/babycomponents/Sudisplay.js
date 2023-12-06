a9be68b74cc2b9eb24b639c510c0bb2f6ee665a2
Jasim <abir_jasim@yahoo.com>
2023-12-06 15:59:59 +0000

    fetch test merge

--- final-project/src/app/babycomponents/Sudisplay.js
+++ final-project/src/app/babycomponents/Sudisplay.js
@@ -1,8 +1,10 @@
-export default function SuDisplay({user_id, first_name, last_name}){
-    return(
-        <>
-        <h2>{first_name} {last_name}</h2>
-        <p>text</p>
-        </>
-    )
+export default function SuDisplay({ user_id, first_name, last_name }) {
+  return (
+    <>
+      <h2>
+        {first_name} {last_name}
+      </h2>
+      <p>text</p>
+    </>
+  );
 }
