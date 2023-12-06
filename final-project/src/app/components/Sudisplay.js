da9c56530e89feb79be5784629a6631a63b4e024
Sara Thampi <saraannthampi@gmail.com>
2023-12-06 12:27:57 +0000

    display su profile

---
+++ final-project/src/app/components/Sudisplay.js
@@ -0,0 +1,8 @@
+export default function SuDisplay({user_id, first_name, last_name}){
+    return(
+        <>
+        <h2>{first_name} {last_name}</h2>
+        <p>text</p>
+        </>
+    )
+}
--- final-project/src/app/viewsuprofile/[id]/page.js
+++ final-project/src/app/viewsuprofile/[id]/page.js
@@ -1,15 +1,38 @@
 
 import Link from 'next/link'
-import Image from 'next/image'
 import BackButton from "../../components/BackButton.jsx"
+import { createClient } from '@supabase/supabase-js';
+import SuDisplay from '@/app/components/Sudisplay.js';
+
+const supaurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+//uses the supabase client to connect to supabase
+const supabase = createClient(supaurl, supakey);
+
+async function getSuProfile() {
+    const res = await supabase.from('service_users').select('*');
+    const data = res;
+    
+    return data;
+};
 
 
-export default function ViewSUProfile() {
-  
+
+
+
+export default async function ViewSUProfile() {
+ const suProfiles = await getSuProfile();
+ console.log("su profile", suProfiles);
+ console.log()
     return (
         <main>
         <BackButton/>
             <h1>View SU Profile</h1>
+            {suProfiles.map(({user_id, first_name, last_name}) => (
+                <SuDisplay key={user_id} first_name={first_name} last_name={last_name}  />
+            ))
+
+             }
             <Link href="/editsu">
                 <button>Edit Service User Button</button>
             </Link>
