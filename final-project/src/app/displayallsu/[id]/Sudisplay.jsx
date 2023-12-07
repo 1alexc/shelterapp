37931c2fc5212df0ce96a11f02735266c1a7ac77
James Diffey <jamesdiffey8@gmail.com>
2023-12-07 14:23:53 +0000

    setting up boilerplate in referral links page

--- final-project/src/app/displayallsu/[id]/Sudisplay.jsx
+++ final-project/src/app/displayallsu/[id]/Sudisplay.jsx
@@ -1,4 +1,6 @@
 //because generatestaticparams was used in the parent component, you will probably need to restart the server for the display to update any changes made here. 
+import './sudisplay.css'
+
 
 export default function SuDisplay({ first_name, last_name, age, gender, dob, ni_number, phone, email, emergency_contact_name, emergency_contact_relationship, emergency_contact_phone }) {
     return (
--- final-project/src/app/pagecomponents/ReferralLinksComp.jsx
+++ final-project/src/app/pagecomponents/ReferralLinksComp.jsx
@@ -1,10 +1,91 @@
-import compStyles from "./referrallinks.css"
+import compStyles from "./referrallinks.css";
+
+import Link from "next/link";
 
 export default function ReferralLinksComp() {
-    return (
-        <>
-            <div className="referral">
-                "this is the referral page"
+  return (
+    <>
+    <div className="page-container">
+    {/* WELCOME BOX */}
+        <div className="flexbox-container-w">
+            <div className="flexbox-item-image">image</div>
+            <div className="flexbox-item-serviceusername">serviceusername</div>
+        </div>
+    {/* NEXT BOX */}
+        <div className="flexbox-container-info">
+            <div className="flexbox-item-title">Title e.g. strengths</div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">age</div>
+                <div className="value">27</div>
             </div>
-        </>
-    )}
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+        </div>
+    {/* NEXT BOX */}
+        <div className="flexbox-container-info">
+            <div className="flexbox-item-title">Accomodation info</div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">age</div>
+                <div className="value">27</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">key</div>
+                <div className="value">value</div>
+            </div>
+        </div>
+    </div>
+
+
+      <div className="page-container">
+        <div className="flexbox-container-w">
+          <p className="flexbox-item-w">Welcome XXX to your Dashboard</p>
+        </div>
+        <div className="flexbox-container-links">
+          <Link href="/displayallsu" passHref legacyBehavior>
+            <div className="flexbox-item-links-box">
+              <img className="item-links-img" src="/icons8-view-32.png" alt="View Service User Icon" />
+              <p className="item-links-text">View Service Users</p>
+            </div>
+          </Link>
+          <Link href="/addsu" passHref legacyBehavior>
+            <div className="flexbox-item-links-box">
+              <img className="item-links-img"  src="icons8-add-male-user-64.png"
+                  alt="Add Service User Icon" />
+              <p className="item-links-text">Add Service User</p>
+            </div>
+          </Link>
+          <Link href="/referrallinks" passHref legacyBehavior>
+            <div className="flexbox-item-links-box">
+              <img className="item-links-img" src="icons8-referral-24.png"alt="Referral Link Button" />
+              <p className="item-links-text">Referral Links</p>
+            </div>
+          </Link>
+          <Link href="/" passHref legacyBehavior>
+            <div className="flexbox-item-links-box">
+              <img className="item-links-img" src="icons8-sign-out-50.png" alt="Sign out Button" />
+              <p className="item-links-text">Sign Out</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
