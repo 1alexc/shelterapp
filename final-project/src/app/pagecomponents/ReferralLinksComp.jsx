9686a7e185a9bb9f1d386612ac054aacb29d1f20
Sara Thampi <saraannthampi@gmail.com>
2023-12-07 14:58:09 +0000

    style basic info 1

--- final-project/src/app/pagecomponents/ReferralLinksComp.jsx
+++ final-project/src/app/pagecomponents/ReferralLinksComp.jsx
@@ -9,27 +9,56 @@ export default function ReferralLinksComp() {
     {/* WELCOME BOX */}
         <div className="flexbox-container-w">
             <div className="flexbox-item-image">image</div>
-            <div className="flexbox-item-serviceusername">serviceusername</div>
+            <div className="flexbox-item-serviceusername">Welcome to William Brown's profile</div>
         </div>
     {/* NEXT BOX */}
         <div className="flexbox-container-info">
-            <div className="flexbox-item-title">Title e.g. strengths</div>
+            <div className="flexbox-item-title">Basic Information</div>
             <div className="flexbox-item-keyvaluepair">
-                <div className="key">age</div>
-                <div className="value">27</div>
+                <div className="key">First Name:</div>
+                <div className="value">William</div>
             </div>
             <div className="flexbox-item-keyvaluepair">
-                <div className="key">key</div>
-                <div className="value">value</div>
+                <div className="key">Last Name:</div>
+                <div className="value">Brown</div>
             </div>
             <div className="flexbox-item-keyvaluepair">
-                <div className="key">key</div>
-                <div className="value">value</div>
+                <div className="key">Age:</div>
+                <div className="value">35</div>
             </div>
             <div className="flexbox-item-keyvaluepair">
-                <div className="key">key</div>
-                <div className="value">value</div>
+                <div className="key">Gender:</div>
+                <div className="value">Male</div>
             </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">DOB:</div>
+                <div className="value">1987-12-05</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">NI Number:</div>
+                <div className="value">IJ678901E</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">Phone Number:</div>
+                <div className="value">07654321098</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">Email:</div>
+                <div className="value">william.b@example.com</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">Emergency Contact Name:</div>
+                <div className="value">Olivia Brown</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">Emergency Contact Relationship:</div>
+                <div className="value">Aunt</div>
+            </div>
+            <div className="flexbox-item-keyvaluepair">
+                <div className="key">Emergency Contact Phone Number:</div>
+                <div className="value">07654321098</div>
+            </div>
+            
         </div>
     {/* NEXT BOX */}
         <div className="flexbox-container-info">
