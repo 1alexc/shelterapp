c20a10095c20a263fad46512beaea3cd3c0f3a3f
James Diffey <jamesdiffey8@gmail.com>
2023-12-11 10:21:14 +0000

    view all sus successfully route guarded

--- final-project/src/app/displayallsu/page.js
+++ final-project/src/app/displayallsu/page.js
@@ -1,60 +1,12 @@
 "use client";
 //must use client for useffect and usestate. this means data is fetched client-side on this page.
-import Link from "next/link";
+
 import AuthRouter from "../AuthRouter.jsx";
-import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-
-const supaurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-//uses the supabase client to connect to supabase
-const supabase = createClient(supaurl, supakey);
-
-// pages/index.js or components/YourComponent.js
 
 export default function ViewAllSUs() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      //try catch to catch errors
-      try {
-        //supabase-specific syntax for a SQL query .from('service_users').select('*') instead of SELECT * from service_users
-        // data from supabase is fetched as an object - this is deconstructed as data and error
-        const { data, error } = await supabase
-          .from("service_users")
-          .select("*");
-        console.log("data fetched on viewallsus", data);
-        if (error) {
-          throw error;
-        }
-
-        setData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
-    }
-
-    fetchData();
-  }, []);
-  console.log(data);
-
   return (
     <>
       <AuthRouter pageName={"displayallsu"} />
-      <h1>View All Service Users</h1>
-      <p>Click the person below to view their data.</p>
-
-
-      {/* map su data to the buttons - they are links connected to a specific profile */}
-      {data.map((profiles) => (
-        <Link key={profiles.user_id} href={`/displayallsu/${profiles.user_id}`}>
-          <button>
-            {profiles.first_name} {profiles.last_name}
-          </button>
-        </Link>
-      ))}
     </>
   );
 }
--- final-project/src/app/pagecomponents/DisplayAllSUComp.jsx
+++ final-project/src/app/pagecomponents/DisplayAllSUComp.jsx
@@ -1,9 +1,54 @@
 import compStyles from "./displayallsu.css"
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+
+const supaurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+//uses the supabase client to connect to supabase
+const supabase = createClient(supaurl, supakey);
 
 export default function DisplayAllSUComp() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        //try catch to catch errors
+        try {
+          //supabase-specific syntax for a SQL query .from('service_users').select('*') instead of SELECT * from service_users
+          // data from supabase is fetched as an object - this is deconstructed as data and error
+          const { data, error } = await supabase
+            .from("service_users")
+            .select("*");
+          console.log("data fetched on viewallsus", data);
+          if (error) {
+            throw error;
+          }
+  
+          setData(data);
+        } catch (error) {
+          console.error("Error fetching data:", error.message);
+        }
+      }
+  
+      fetchData();
+    }, []);
+    console.log(data);
     return (
         <>
+            <h1>View All Service Users</h1>
+            <p>Click the person below to view their data.</p>
+
             <div className="displayall">
+                {data.map((profiles) => (
+                    <Link key={profiles.user_id} href={`/displayallsu/${profiles.user_id}`}>
+                <button>
+                     {profiles.first_name} {profiles.last_name}
+                </button>
+        </Link>
+      ))}
                 
             </div>
         </>
