c58f841df554e66a1bbf261791f78249e71b3cfc
Sara Thampi <saraannthampi@gmail.com>
2023-12-11 23:25:43 +0000

    tidy up header

--- final-project/src/app/babycomponents/Header.jsx
+++ final-project/src/app/babycomponents/Header.jsx
@@ -11,23 +11,6 @@ const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supaurl, supakey);
 
 
-// const session = supabase.auth.session();
-// if (session) {
-//   const { data: { user } } = await supabase.auth.getUser();
-//   console.log("userdetails", user);
-// } else {
-//   console.log("User is not logged in");
-// }
-
-
-// const { data, error } = await supabase.auth.getSession()
-// console.log(data);
-
-
-// const { data, error } = await supabase.auth.getUserIdentities()
-// console.log("user", data);
-
-
 export default function Header() {
   return (
       <div className="flexbox-container">
