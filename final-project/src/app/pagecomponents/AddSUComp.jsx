f7bcfc330be22a4366bc7d08eede2e7937b47405
Jasim <abir_jasim@yahoo.com>
2023-12-13 12:23:51 +0000

    working table

--- final-project/src/app/pagecomponents/AddSUComp.jsx
+++ final-project/src/app/pagecomponents/AddSUComp.jsx
@@ -8,14 +8,18 @@ const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 //uses the supabase client to connect to supabase
 const supabase = createClient(supaurl, supakey);
+//, last_name:"", age:"", gender:"", dob:"", ni_number:"", phone:"", emergency_contact_name:"", emergency_contact_relationship:"", email:"", emergency_contact_phone:""
+//, last_name, age, gender, dob, ni_number, phone, emergency_contact_name, emergency_contact_relationship, email, emergency_contact_phone
+
+
 
 export default function AddSUComp() {
     // post array that will handle the post return from the superbase api
     const [posts, setPosts] = useState([])
     //singular post variable to store the user input in coloumn
-    const [post, setPost] = useState({user_id:"", first_name:"", last_name:"", age:"", gender:"", dob:"", ni_number:"", phone:"", emergency_contact_name:"", emergency_contact_relationship:"", email:"", emergency_contact_phone:""})
+    const [post, setPost] = useState({user_id:"", first_name:""})
     //destructure the coloum from the table
-    const {user_id, first_name, last_name, age, gender, dob, ni_number, phone, emergency_contact_name, emergency_contact_relationship, email, emergency_contact_phone} = post
+    const {user_id, first_name} = post
     
     useEffect(()=>{
         fetchPosts()
@@ -32,16 +36,36 @@ export default function AddSUComp() {
         await supabase
             .from("tablename")
             .insert([
-                {user_id, first_name, last_name, age, gender, dob, ni_number, phone, emergency_contact_name, emergency_contact_relationship, email, emergency_contact_phone}
+                {user_id, first_name}
             ])
             .single()
-            setPost({user_id:"", first_name:"", last_name:"", age:"", gender:"", dob:"", ni_number:"", phone:"", emergency_contact_name:"", emergency_contact_relationship:"", email:"", emergency_contact_phone:""})
+            setPost({user_id:"", first_name:""})
             fetchPosts()
     }
 
     return (
-        <div>
-            <h1 className="add-title">hi from the add su component</h1>
+        <div className="App">
+            <input
+                placeholder="Title"
+                value={title}
+                onChange={e => setPost({...post, title: e.target.value})}
+                />
+            <input
+                placeholder="Content"
+                value={content}
+                onChange={e => setPost({...post, content: e.target.value})}
+                />
+            <button onClick={createPost}>Create Post</button>
+            {
+                posts.map(post =>(
+                    <div key= {post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.content}</p>
+                    </div>
+                 ))
+            }
+            
+           
         </div>
     )
 }
